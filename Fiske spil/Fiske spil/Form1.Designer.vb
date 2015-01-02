b30f082985e69caca07a4697bcaf0d0d88abb015
jonatan27 <klitsamuel11@gmail.com>
2015-01-02 13:49:50 +0100

    Besked

--- Fiske spil/Fiske spil/Form1.Designer.vb
+++ Fiske spil/Fiske spil/Form1.Designer.vb
@@ -27,7 +27,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(27, 191)
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
